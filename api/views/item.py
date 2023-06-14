8aa0ae66013441eb1276d9abf5824ea1c2e17050
MASANORI-M <kougakumasa.1031@gmail.com>
2023-06-14 23:06:50 +0900

    second

--- api/views/item.py
+++ api/views/item.py
@@ -1,17 +1,17 @@
-# from flask import Flask, render_template, request, Blueprint
-# from ..models.item import Item
+from flask import Flask, render_template, request, Blueprint
+from ..models.item import Item
 
-# item = Blueprint('item_router', __name__)
+item = Blueprint('item_router', __name__)
 
-# @item.route("/")
-# @item.route("/index")
-# def index():
-#     name = request.args.get('name')
-#     books = ["化学", "物理", "数学", "情報"]
-#     return render_template("index.html", name = name, books = books)
+@item.route("/")
+@item.route("/index")
+def index():
+    name = request.args.get('name')
+    books = ["化学", "物理", "数学", "情報"]
+    return render_template("index.html", name = name, books = books)
 
-# @item.route("/index", methods=["post"])
-# def post():
-#     name = request.form["name"]
-#     books = ["化学", "物理", "数学", "情報"]
-#     return render_template("index.html", name = name, books = books)
+@item.route("/index", methods=["post"])
+def post():
+    name = request.form["name"]
+    books = ["化学", "物理", "数学", "情報"]
+    return render_template("index.html", name = name, books = books)
